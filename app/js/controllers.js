0b5da8dac2a3a6df051831d017c5054e9ff2ab98
dstills <dstills87@gmail.com>
2016-09-27 17:01:04 -0700

    headway on basemap select custom widget and other stuff

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -35,21 +35,70 @@ angular.module('mapItApp')
     this.viewOptions = viewOptions.value;
 
     this.onViewCreated = function(view) {
-      $scope.widgets.forEach(function(widget) {
+      console.log(view);
+      $scope.widgets.forEach(function(widget, i) {
         widget.view = view;
-        view.ui.add(widget, 'top-right');
+        if (i < 3) {
+          view.ui.add(widget, 'top-right');
+        } else {
+          widget.startup();
+        }
       });
     };
+
+    // this.renderLayer = function(deps) {
+    //   var deferred = $q.defer();
+    //   geoService.getFeatures().query().$promise.then(function(features) {
+    //     var featureLayer = new deps['FeatureLayer']({
+    //       source: (function() {
+    //         var source = [];
+    //         for (var i = 0; i <= features.length; i++) {
+    //           var f = Terraformer.ArcGIS.convert(features[i]);
+    //           source.push(
+    //             new Graphic({
+    //               attributes: f.attributes,
+    //               geometry: new deps['Point'](f.geometry),
+    //               symbol: new deps['SimpleMarkerSymbol']()
+    //             })
+    //           );
+    //         }
+    //         return source;
+    //       })(),
+    //       fields: [
+    //         new deps['Field']({ name: '_id', alias: 'ID', type: 'oid' }),
+    //         new deps['Field']({ name: 'name', alias: 'Name', type: 'string' }),
+    //         new deps['Field']({ name: 'category', alias: 'Category', type: 'string' }),
+    //         new deps['Field']({ name: 'description', alias: 'Description', type: 'string' })
+    //       ],
+    //       objectIdField: '_id',
+    //       spatialReference: new deps['SpatialReference']({
+    //         wkid: 4326
+    //       }),
+    //       geometryType: 'point',
+    //       popupTemplate: new deps['PopupTemplate']({
+    //         title: '<h3>{name} <small>{category}</small></h3>',
+    //         content: '<p>{description}</p>'
+    //       }),
+    //       id: 'points'
+    //     });
+    //     deferred.resolve(featureLayer);
+    //     // map.add(featureLayer);
+    //   });
+    //   return deferred.promise;
+    // };
 
     // this.startMap = function() {
       esriLoader.require([
         'esri/Map',
         'dojo/_base/array',
         'dojo/dom',
+        'dojo/dom-construct',
         'esri/layers/FeatureLayer',
         'esri/layers/GraphicsLayer',
+        'esri/layers/StreamLayer',
         'esri/Graphic',
         'esri/geometry/Point',
+        'esri/renderers/SimpleRenderer',
         'esri/geometry/SpatialReference',
         'esri/symbols/SimpleMarkerSymbol',
         'esri/layers/support/Field',
@@ -63,10 +112,13 @@ angular.module('mapItApp')
         Map,
         array,
         dom,
+        domConstruct,
         FeatureLayer,
         GraphicsLayer,
+        StreamLayer,
         Graphic,
         Point,
+        SimpleRenderer,
         SpatialReference,
         SimpleMarkerSymbol,
         Field,
@@ -77,19 +129,77 @@ angular.module('mapItApp')
         Search,
         Zoom
       ) {
+        // var streamLayer = new StreamLayer({
+        //   url: 'http://localhost:3443/features/',
+        //   renderer: new SimpleRenderer({
+        //     symbol: new SimpleMarkerSymbol()
+        //   }),
+        //   fields: [
+        //     new Field({ name: '_id', alias: 'ID', type: 'oid' }),
+        //     new Field({ name: 'name', alias: 'Name', type: 'string' }),
+        //     new Field({ name: 'category', alias: 'Category', type: 'string' }),
+        //     new Field({ name: 'description', alias: 'Description', type: 'string' })
+        //   ],
+        //   objectIdField: '_id',
+        //   spatialReference: new SpatialReference({
+        //     wkid: 4326
+        //   }),
+        //   geometryType: 'point',
+        //   popupTemplate: new PopupTemplate({
+        //     title: '<h3>{name} <small>{category}</small></h3>',
+        //     content: '<p>{description}</p>'
+        //   }),
+        //   purgeOptions: {
+        //     displayCount: 1000
+        //   }
+        // });
+        // Create the map
         this.map = new Map({
           basemap:'topo',
           ground: 'world-elevation'
+          // ,layers: [streamLayer]
         });
+        // Custom javascript class for BasemapSelect widget
+        var BasemapSelect = (function(map) {
+          function BasemapSelect(options, domNode) {
+            this.options = options;
+            this.node = domNode;
+
+            this._template =
+            // '<div class="esri-widget" widgetid="'+domNode.id+'">'+
+              '<select id="basemap-select">'+
+                '<option value="streets" selected>Streets</option>'+
+                '<option value="hybrid">Satellite</option>'+
+                '<option value="topo">Topographic</option>'+
+                '<option value="terrain">Streets</option>'+
+                '<option value="gray">Gray</option>'+
+                '<option value="national-geographic">National Geographic</option>'+
+                '<option value="streets">Streets</option>'+
+              '</select>'
+            // +'</div>';
+            this._started = false;
+            this._selected = map.basemap;
+          }
+          BasemapSelect.prototype.select = function(value) {
+            map.basemap = value;
+          };
+          BasemapSelect.prototype.startup = function() {
+            this._started = true;
+            var node = domConstruct.toDom(this._template);
+            console.log(node);
+            this.node.appendChild(node);
+            console.log('basemapselect started up!', map);
+          };
+          return BasemapSelect;
+        })(this.map);
+        // Grab widgets and put them in scope
         $scope.widgets.push(new Compass());
         $scope.widgets.push(new Zoom());
         $scope.widgets.push(new Track());
-        var legend = new Legend({
-          view: this.map.view
-        }, dom.byId('legend'));
-        var search = new Search({
-          view: this.map.view
-        }, dom.byId('search'));
+        $scope.widgets.push(new Legend({}, dom.byId('legend')));
+        $scope.widgets.push(new Search({}, dom.byId('search')));
+        $scope.widgets.push(new BasemapSelect({}, dom.byId('basemaps')));
+        // query the server and create the feature class
         geoService.getFeatures().query().$promise.then(function(features) {
           var featureLayer = new FeatureLayer({
             source: array.map(features, function(feature) {
@@ -115,7 +225,8 @@ angular.module('mapItApp')
             popupTemplate: new PopupTemplate({
               title: '<h3>{name} <small>{category}</small></h3>',
               content: '<p>{description}</p>'
-            })
+            }),
+            id: 'points'
           });
           this.map.add(featureLayer);
         }.bind(this));
@@ -140,6 +251,9 @@ angular.module('mapItApp')
     }, {
       title: 'Legend',
       widgetName: 'legend'
+    }, {
+      title: 'Basemaps',
+      widgetName: 'basemaps'
     }];
     $scope.panels.map(function(panel) {
       panel.id = idCount;
--- app/js/polyfills.js
+++ app/js/polyfills.js
@@ -4,7 +4,6 @@
   var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
   var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
   var concat = Function.bind.call(Function.call, Array.prototype.concat);
-  // var keys = Reflect.ownKeys;
 
   if (!Object.values) {
     Object.values = function(O) {
@@ -30,121 +29,84 @@
 
   angular.module('esri.map')
     .directive('myEditButtons', ['geoService', '$uibModal', function(geoService, $uibModal) {
-          return {
-            require: '^^esriSceneView',
-            template: 
-            '<div class="map-map-buttons btn-group">'+
-              '<label class="btn btn-success" uib-btn-radio="\'Left\'" ng-model="editButtonModel" uncheckable><i class="fa fa-map-marker"></i></label>'+
-              '<label class="btn btn-danger" uib-btn-radio="\'Right\'" ng-model="editButtonModel" uncheckable><i class="fa fa-eraser"></i></label>'+
-            '</div>',
-            restrict: 'E',
-            scope: {
-              view: '@'
-            },
-            link: function (scope, element, attrs, vm) {
-              element.on('click', function(e) {
-                var btnClicked = scope.editButtonModel;
-                console.log(btnClicked);
-                if (btnClicked === null) {
-                  scope.editButtonModel = '';
-                  scope.$digest();
-                  handle = null;
-                  return;
-                }
-                var handle = vm.view.on('click', function(e) {
-                  if (btnClicked === 'Left') {
-                    var point = {};
-                    point.geometry = Terraformer.ArcGIS.parse({
-                      x: e.mapPoint.x,
-                      y: e.mapPoint.y,
-                      spatialReference: {
-                        wkid: e.mapPoint.spatialReference.wkid
-                      }
-                    });
-                    $uibModal.open({
-                      templateUrl: '../views/map/modal.html',
-                      controller: function() {
-                        var $ctrl = this;
-
-                        $ctrl.close = function() {
-                          $uibModal.dismiss('cancel');
-                        };
-                      }
-                    });
-                    point.properties = {
-                      name: 'test',
-                      category: 'miscellaneous'
-                    };
-                    var features = geoService.getFeatures();
-                    features.query(function(arg) {
-                      point.type = "Feature";
-                      features.add(point);
-                      console.log(features);
-                    });
-                  } else if (btnClicked === 'Right') {
-                    console.log('Select point at ', e.mapPoint);
-                  }
-                  scope.editButtonModel = '';
-                  scope.$digest();
-                  handle.remove();
-                });
-                // var btnClicked = scope.editButtonModel;
-                // if (btnClicked === 'Left') {
-                //   var handle = vm.view.on('click', function(e) {
-                //     var point = {};
-                //     point.geometry = Terraformer.ArcGIS.parse({
-                //       x: e.mapPoint.x,
-                //       y: e.mapPoint.y,
-                //       spatialReference: {
-                //         wkid: e.mapPoint.spatialReference.wkid
-                //       }
-                //     });
-                //     $uibModal.open({
-                //       templateUrl: '../views/map/modal.html',
-                //       controller: function() {
-                //         var $ctrl = this;
-
-                //         $ctrl.close = function() {
-                //           $uibModal.dismiss('cancel');
-                //         };
-                //       }
-                //     });
-                //     point.properties = {
-                //       name: 'test',
-                //       category: 'miscellaneous'
-                //     };
-                //     var features = geoService.getFeatures();
-                //     features.query(function(arg) {
-                //       point.type = "Feature";
-                //       features.add(point);
-                //       console.log(features);
-                //     });
-                //     console.log(point);
-                //     scope.editButtonModel = '';
-                //     scope.$digest();
-                //     handle.remove();
-                //   });
-                // } else if (btnClicked === 'Right') {
-                //   var handle = vm.view.on('click', function(e) {
-                //     var point = {};
-                //     point.geometry = Terraformer.ArcGIS.parse({
-                //       x: e.mapPoint.x,
-                //       y: e.mapPoint.y,
-                //       spatialReference: {
-                //         wkid: e.mapPoint.spatialReference.wkid
-                //       }
-                //     });
-                //     console.log(point);
-                //     scope.editButtonModel = '';
-                //     scope.$digest();
-                //     handle.remove();
-                //   });
-                // } else {
-                //   return;
-                // }
-              });
+      return {
+        require: '^^esriSceneView',
+        template: 
+        '<div class="map-map-buttons btn-group">'+
+          '<label class="btn btn-success" uib-btn-radio="\'Left\'" ng-model="editButtonModel" uncheckable><i class="fa fa-map-marker"></i></label>'+
+          '<label class="btn btn-danger" uib-btn-radio="\'Right\'" ng-model="editButtonModel" uncheckable><i class="fa fa-eraser"></i></label>'+
+        '</div>',
+        restrict: 'E',
+        scope: {
+          view: '@'
+        },
+        link: function (scope, element, attrs, vm) {
+          var handles = [];
+          element.on('click', function(e) {
+            var btnClicked = scope.editButtonModel;
+            console.log(btnClicked);
+            if (btnClicked === null) {
+              var handle = handles.pop();
+              handle.remove();
+              scope.editButtonModel = '';
+              scope.$digest();
+              return;
             }
-          };
-        }])
+            var handle = vm.view.on('click', function(e) {
+              if (btnClicked === 'Left') {
+                var point = {
+                  type: 'Feature',
+                  geometry: Terraformer.ArcGIS.parse({
+                    x: e.mapPoint.x,
+                    y: e.mapPoint.y,
+                    spatialReference: {
+                      wkid: e.mapPoint.spatialReference.wkid
+                    }
+                  }),
+                  properties: {}
+                };
+                var propertiesModal = $uibModal.open({
+                  templateUrl: '../views/map/modal.html',
+                  controller: function($scope) {
+                    var features = geoService.getFeatures();
+                    console.log(this);
+
+                    $scope.properties = {
+                      name: '',
+                      category: '',
+                      description: ''
+                    };
+
+                    $scope.submitFeature = function(e) {
+
+                      point.properties = {
+                        name: $scope.properties.name,
+                        category: $scope.properties.category.toLowerCase(),
+                        description: $scope.properties.description
+                      };
+                      console.log(point);
+                      features.query(function(arg) {
+                        features.add(point);
+                      });
+                      propertiesModal.close();
+                    };
+
+                    $scope.cancel = function() {
+                      propertiesModal.close();
+                    };
+                  }
+                });
+              } else if (btnClicked === 'Right') {
+                console.log('Select point at ', e.mapPoint);
+              }
+              scope.editButtonModel = '';
+              scope.$digest();
+              handle.remove();
+            });
+            handles.push(handle);
+          });
+        }
+      };
+    }])
   ;
 })(angular);
